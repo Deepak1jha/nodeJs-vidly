c19085e852913d508c58182dfd1a961616eeec8d
Deepak Jha <deepak@bonamisoftware.com>
2020-11-21 23:42:46 +0530

    MongoDb connection created

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const config = require('config');
 const logger = require('morgan');
 const helmet = require('helmet');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log("Connected to mongoDb database"))
+    .catch((error) => {
+        console.error("cant connect to mongoDB",error);
+    });
 const bookRouter = require('./routes/book/bookRoute')
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use('/api/books',bookRouter);
+app.use('/api/books', bookRouter);
 
 if (app.get(`env`) === `development`) {
     console.log(config.get('name'));
