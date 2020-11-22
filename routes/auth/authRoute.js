47f1079b28394b5cf6b806a96a4f2c823cc936b8
Deepak Jha <deepak@bonamisoftware.com>
2020-11-22 17:25:00 +0530

    Jwt token added

--- routes/auth/authRoute.js
+++ routes/auth/authRoute.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 const User = require('../../model/user/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post('/login', async (req, res) => {
     let user = await User.findOne({email: req.body.email});
     if (!user) return res.status(400).send("Invalid username and password");
     const isValidPassword = await bcrypt.compare(req.body.password, user.password);
     if (!isValidPassword) return res.status(400).send("Invalid username and password");
-    res.send(true);
+    const token = jwt.sign({_id: user._id}, 'jwtPrivateKey');
+    res.send(token);
 });
 
 module.exports = router;
