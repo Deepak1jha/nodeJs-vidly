8e6bb7e084511245c72104a3bc2143bb72da688d
Deepak Jha <deepak@bonamisoftware.com>
2020-11-22 11:52:42 +0530

    Added enums validators

--- app.js
+++ app.js
@@ -20,15 +20,33 @@ const courseSchema = new mongoose.Schema({
     name: String,
     author: String,
     tags: [String],
+    category: {
+        type: String,
+        required: true,
+        enum: ['Java', 'Javascript', 'React']
+    },
     date: {type: Date, default: Date.now()},
     isPublished: Boolean
 });
 
+const Course = mongoose.model('course', courseSchema);
+const courseObj = new Course({
+    name: "NodeJs",
+    author: "Deepak",
+    category: "Java",
+    tags: ["Advance", "Basic"],
+    isPublished: true
+});
+try {
+    // courseObj.save().then(res => console.log(res));
+} catch (ex) {
+    console.log(ex)
+}
 if (app.get(`env`) === `development`) {
     console.log(config.get('name'));
     app.use(logger('dev'));
 }
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 2000
 app.listen(port, () => {
     console.log(`Listing to this port => ${port}`)
 })
