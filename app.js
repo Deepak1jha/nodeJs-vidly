217e11d3b193d0beb48804fbc41656a1aea91b7d
Deepak Jha <deepak@bonamisoftware.com>
2020-11-22 11:11:34 +0530

    Course schema ic created

--- app.js
+++ app.js
@@ -3,22 +3,32 @@ const config = require('config');
 const logger = require('morgan');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/vidly')
-    .then(() => console.log("Connected to mongoDb database"))
-    .catch((error) => {
-        console.error("cant connect to mongoDB",error);
-    });
+
 const bookRouter = require('./routes/book/bookRoute')
 const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use('/api/books', bookRouter);
 
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log("Connected to mongoDb database"))
+    .catch((error) => {
+        console.error("cant connect to mongoDB", error);
+    });
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now()},
+    isPublished: Boolean
+});
+
 if (app.get(`env`) === `development`) {
     console.log(config.get('name'));
     app.use(logger('dev'));
 }
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 app.listen(port, () => {
     console.log(`Listing to this port => ${port}`)
 })
