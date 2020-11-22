2a4658dfe1d016de5b4bb4ca566ed3df0158ce48
Deepak Jha <deepak@bonamisoftware.com>
2020-11-22 14:56:37 +0530

    Reference type relationship created between documents

--- app.js
+++ app.js
@@ -16,20 +16,54 @@ mongoose.connect('mongodb://localhost/vidly')
         console.error("cant connect to mongoDB", error);
     });
 
-const courseSchema = new mongoose.Schema({
+const Author = mongoose.model('Author', new mongoose.Schema({
     name: String,
-    author: String,
-    tags: [String],
-    category: {
-        type: String,
-        required: true,
-        enum: ['Java', 'Javascript', 'React']
-    },
-    date: {type: Date, default: Date.now()},
-    isPublished: Boolean
-});
+    bio: String,
+    website: String
+}));
+const Course = mongoose.model('course', new mongoose.Schema({
+    name: String,
+    author:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:Author
+    }
+}));
 
-const Course = mongoose.model('course', courseSchema);
+// const courseSchema = new mongoose.Schema({
+//     name: String,
+//     author: String,
+//     tags: [String],
+//     category: {
+//         type: String,
+//         required: true,
+//         enum: ['Java', 'Javascript', 'React']
+//     },
+//     date: {type: Date, default: Date.now()},
+//     isPublished: Boolean
+// });
+async function createAuthor(name, bio, website) {
+    const author = new Author({name, bio, website});
+    const result = await author.save();
+    console.log(result);
+}
+
+async function createCourse(name, author) {
+    const course = new Course({
+        name,
+        author
+    });
+    const result = await course.save();
+    console.log(result);
+}
+
+async function getCourseList() {
+    const courses = await Course
+        .find()
+        // .populate('author')
+        .select('name author');
+    console.log(courses);
+}
+
 const courseObj = new Course({
     name: "NodeJs",
     author: "Deepak",
@@ -39,14 +73,18 @@ const courseObj = new Course({
 });
 try {
     // courseObj.save().then(res => console.log(res));
+    // createAuthor('deepak','student',"deepak1jha.github.io");
+    // createCourse("Java","5fba25aa803ce167451c7d42");
+    getCourseList();
 } catch (ex) {
     console.log(ex)
 }
+
 if (app.get(`env`) === `development`) {
     console.log(config.get('name'));
     app.use(logger('dev'));
 }
-const port = process.env.PORT || 2000
+const port = process.env.PORT || 2072
 app.listen(port, () => {
     console.log(`Listing to this port => ${port}`)
 })
